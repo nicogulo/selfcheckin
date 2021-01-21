b643a9b5d09245866aaa88ccadb634d81555770e
nicogulo <63594315+nicogulo@users.noreply.github.com>
2021-01-22 01:11:20 +0700

    Added random number

--- js/index.js
+++ js/index.js
@@ -11,3 +11,20 @@ function handleSubmit () {
 
     return;
 }
+function maketextnumber(n) {
+
+    for (var r = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], e = n, t = new Array, a = 0; a <= e - 1; a++) {
+
+        t[a] = r[parseInt(Math.random() * r.length)];
+
+        t = t;
+
+        randomtextnumber = t.join("")
+
+    }
+
+}
+
+var hasil = document.getElementById("hasil-output");
+
+hasil.innerHTML = (maketextnumber(6), randomtextnumber);
